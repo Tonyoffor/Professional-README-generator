53aabf503698c258e0e40b03d3a7d19f387014c9
Tonyoffor <tonyc.offor@gmail.com>
2022-12-05 13:06:13 -0500

    edits

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,14 +1,25 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  [![License: Open Font-1.1](https://img.shields.io/badge/License-OFL_1.1-lightgreen.svg)](https://opensource.org/licenses/OFL-1.1)
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+
+}
+
+function generateReadMe(response){
+  return 
+  <h1 align="center">${response.Title}</h1>
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
